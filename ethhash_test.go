5c61c855534425016b63031f91f4d623e65c0047
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-10-11 21:42:32 +0900

    test: improve

--- ethhash_test.go
+++ ethhash_test.go
@@ -12,6 +12,33 @@ import (
 	"github.com/m0t0k1ch1-go/nullable/internal/testutil"
 )
 
+func TestEthHashNullableString(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		tcs := []struct {
+			name string
+			in   nullable.EthHash
+			out  nullable.String
+		}{
+			{
+				"null",
+				nullable.NewEthHash(ethcommon.Hash{}, false),
+				nullable.NewString("", false),
+			},
+			{
+				"not null",
+				nullable.NewEthHash(ethcommon.HexToHash("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"), true),
+				nullable.NewString("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f", true),
+			},
+		}
+
+		for _, tc := range tcs {
+			t.Run(tc.name, func(t *testing.T) {
+				testutil.Equal(t, tc.out, tc.in.NullableString())
+			})
+		}
+	})
+}
+
 func TestEthHashValue(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		tcs := []struct {
